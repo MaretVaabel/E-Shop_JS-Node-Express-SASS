d77b4ee50968abb04dd8c16326271e4c208f71f3
Maret <maret3@gmail.com>
2024-11-29 12:38:39 +0200

    Improve- added a bit styles and comments

--- ylesanne3-dom/views/allProductsView.js
+++ ylesanne3-dom/views/allProductsView.js
@@ -1,26 +1,32 @@
 // Toodete vaate genereerimine
+
 export const displayAllProductsView = (products) => {
-  const container = document.getElementById("products-view");
+  const container = document.getElementById("main-container");
+
   container.innerHTML = "<h2>Tooted</h2>";
 
   const productsContainer = document.createElement("div");
   productsContainer.classList.add("products-container");
 
   products.forEach((product) => {
-    const productElement = document.createElement("div");
-    productElement.classList.add("product");
-    productElement.innerHTML = `
+    const productCard = document.createElement("div");
+    productCard.classList.add("product");
+    productCard.innerHTML = `
         <h3>${product.name}</h3>
         <p>Kategooria: ${product.category}</p>
         <p>Hind: $${product.price}</p>
       `;
 
-    productElement.onclick = (e) => {
+    // toote kaardile vajutades mine toode detaisesse vaatesse
+    productCard.onclick = (e) => {
       e.stopPropagation();
       navigateTo("product-detail");
     };
 
-    productsContainer.append(productElement);
+    //ühe toote kaardi lisan toodete konteinerisse
+    productsContainer.append(productCard);
   });
+
+  // Tooted lisan main kontainersisse
   container.append(productsContainer);
 };
